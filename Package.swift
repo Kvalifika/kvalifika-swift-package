e1d2993dd31aa6a7cf66401a64b0064993a8982e
Mirian Okradze <okradzemirian@gmail.com>
2021-06-14 17:29:31 +0400

    facetec xcframework

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
                       url: "https://kvalifika-ios.s3.eu-central-1.amazonaws.com/sdk/Kvalifika-0.0.5.xcframework.zip",
                       checksum: "87f68b0f992827ddee5f5a1716cbaf4d14abd38ca6232ba008065000c7caba24"),
         .binaryTarget(name: "FaceTecSDK",
-                      url: "https://kvalifika-ios.s3.eu-central-1.amazonaws.com/facetec/FaceTecSDK-9.0.4.zip",
-                      checksum: "7481df470f85233907e622399f9cf9d081acda019e68f24fe2ad6b6a76f33edb"),
+                      url: "https://kvalifika-ios.s3.eu-central-1.amazonaws.com/facetec/FaceTecSDK-9.0.4.xcframework.zip",
+                      checksum: "6431910f03e486e5c3d9041ec4e5c01313e585aae0882e5f4a14ec4d995f637d"),
     ]
 )
